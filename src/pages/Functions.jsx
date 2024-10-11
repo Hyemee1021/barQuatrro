baf1b57c40ea0895f132b4f55ab862a38e39b6b9
Hye Mee Park <hyemee1021@gmail.com>
2024-10-11 14:08:26 +1100

    on process in hero section on functions page

--- src/pages/Functions.jsx
+++ src/pages/Functions.jsx
@@ -1,7 +1,20 @@
 import React from "react";
-
+import functionP from "../assets/images.jpeg";
 const Functions = () => {
-  return <div>Functions</div>;
+  return (
+    <main>
+      <div className="relative">
+        <img
+          className="w-full object-cover h-[30vh] md:h-[70vh]"
+          src={functionP}
+          alt="hero pic"
+        />
+        <p className="absolute font-bold font-3xl  top-[50%] text-white title left-[50%]  transform -translate-x-1/2 -translate-y-1/2 ">
+          What's On
+        </p>
+      </div>
+    </main>
+  );
 };
 
 export default Functions;
