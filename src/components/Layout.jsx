11de2874ded93b14c154b904d05ddfffde7ec037
Hye Mee Park <hyemee1021@gmail.com>
2024-10-30 18:10:26 +1100

    backtotop componenet issue fixed

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import { BackToTop } from "./BackToTop";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
@@ -8,6 +9,7 @@ const Layout = () => {
     <div className="w-full">
       <div className="w-full mx-auto">
         <Header />
+        <BackToTop />
         <Outlet />
         <Footer />
       </div>
